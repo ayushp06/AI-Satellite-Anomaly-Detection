35371e11f8de912a2e6061f5b8f610dbb3e35226
alter <apandejee006@yahoo.com>
2025-10-19 23:17:35 -0400

    Intilized telemetry file to log time, quaternion, and angular velocity

---
+++ src/sim/telemetry.py
@@ -0,0 +1,7 @@
+#Intilizes time, quaternion, and angular velocity to be logged
+def teleBuild(t, q, w):
+    return {
+        "t" : round(float(t), 3),
+        "q" : [float(x) for x in q],
+        "w" : [float(val) for val in w]
+    }
