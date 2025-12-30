bfdd2ce56646a8b30aeffb6b33678f812f26fd19
Aryan <aryanpx5@gmail.com>
2025-12-30 15:38:06 -0500

    phase 1
    buns

--- src/sim/telemetry.py
+++ src/sim/telemetry.py
@@ -1,8 +1,8 @@
 #Initializes time, quaternion, and angular velocity to be logged
 def teleBuild(t, q, w, faultFlag = False):
     return {
-        "t" : round(float(t), 3),
+        "t" : float(t),
         "q" : [float(x) for x in q],
         "w" : [float(val) for val in w],
-        "fault" : int(faultFlag)
+        "fault" : int(bool(faultFlag))
     }
